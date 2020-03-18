652518f1eb1a73f113e4f6bbd3e0fe02e2476bce
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2020-03-18 16:34:17 -0300

    First Commit

---
+++ src/main/kotlin/spring/studies/captcha/validation/CaptchaValidationApplication.kt
@@ -0,0 +1,11 @@
+package spring.studies.captcha.validation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CaptchaValidationApplication
+
+fun main(args: Array<String>) {
+	runApplication<CaptchaValidationApplication>(*args)
+}
---
+++ src/test/kotlin/spring/studies/captcha/validation/CaptchaValidationApplicationTests.kt
@@ -0,0 +1,13 @@
+package spring.studies.captcha.validation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CaptchaValidationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
